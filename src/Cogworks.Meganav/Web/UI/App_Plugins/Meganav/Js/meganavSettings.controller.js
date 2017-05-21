bb4cf33d04e48e30212758ae2efb5d771fee2cc2
Callum Whyte <hey@callumwhyte.com>
2017-05-21 18:09:53 +0100

    Replacing Umbraco Link Picker with custom overlay to allow for further modification

---
+++ src/Cogworks.Meganav/Web/UI/App_Plugins/Meganav/Js/meganavSettings.controller.js
@@ -0,0 +1,19 @@
+﻿function MeganavSettings($scope, $controller) {
+
+    $scope.dialogOptions = {
+        currentTarget: null
+    };
+
+    if ($scope.model.value) {
+        $scope.dialogOptions.currentTarget = $scope.model.value;
+    }
+
+    $scope.$on("formSubmitting", function (ev, args) {
+        $scope.model.value = $scope.target;
+    });
+
+    // load Umbraco Link Picker controller
+    $controller('Umbraco.Dialogs.LinkPickerController', { $scope: $scope });
+}
+
+angular.module("umbraco").controller("Cogworks.Meganav.MeganavSettingsController", MeganavSettings);
