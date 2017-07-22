64f2f1340ed22dedd95aef6ccf5ffa421e5069ba
Callum Whyte <callum.whyte@thecogworks.com>
2017-07-23 00:26:05 +0100

    Adding helper to get property editor settings

---
+++ src/Cogworks.Meganav/Helpers/PreValueHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core;
+
+namespace Cogworks.Meganav.Helpers
+{
+    public static class PreValueHelper
+    {
+        public static IDictionary<string, string> GetPreValues(int dataTypeId)
+        {
+            var context = ApplicationContext.Current;
+            var dataTypeService = context.Services.DataTypeService;
+
+            var preValueCollection = dataTypeService.GetPreValuesCollectionByDataTypeId(dataTypeId);
+
+            return preValueCollection.PreValuesAsDictionary.ToDictionary(x => x.Key, x => x.Value.Value);
+        }
+    }
+}
