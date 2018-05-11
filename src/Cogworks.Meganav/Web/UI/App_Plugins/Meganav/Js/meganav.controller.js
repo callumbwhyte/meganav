f079c253400fd43abe73bcc496a885007090bff2
Robin Neal <robin@visarc.uk>
2018-05-11 11:20:35 +0100

    Fix IE 11 compat issues (remove es6)

--- src/Cogworks.Meganav/Web/UI/App_Plugins/Meganav/Js/meganav.controller.js
+++ src/Cogworks.Meganav/Web/UI/App_Plugins/Meganav/Js/meganav.controller.js
@@ -1,14 +1,14 @@
 ﻿function Meganav($scope, meganavResource) {
-
+  
   $scope.items = [];
-    
+  
   // Configure UI Tree Options
   $scope.treeOptions = {
     dropped: function (e) {
       setLevel(e.dest.nodesScope.depth(), e.source.nodeScope);
     }
   }
-
+  
   function setLevel(depth, node) {
     if (node) {
       if (angular.isObject(node.$modelValue)) {
@@ -19,19 +19,19 @@
       }
     }
   }
-
+  
   // Store properties config
   var propertyFields = $scope.model.config.properties || [];
-
+  
   // Initial Setup
   if ($scope.model.value) {
     // retreive the saved items
     $scope.items = $scope.model.value;
-
+    
     // get updated entities for content
     getItemEntities($scope.items);
   }
-
+  
   // Add new item
   $scope.add = function () {
     openSettings({}, function (model) {
@@ -39,7 +39,7 @@
       $scope.items.push(buildNavItem(model.value));
     });
   };
-
+  
   // Edit item
   $scope.edit = function (item) {
     openSettings(item, function (model) {
@@ -48,20 +48,22 @@
       buildNavItem(model.value, item);
     });
   };
-
+  
   // Remove item
   $scope.remove = function (item) {
     item.remove();
   };
-
+  
   $scope.isVisible = function (item) {
-      return $scope.model.config.removeNaviHideItems == true ? item.naviHide !== true : true;
+    return $scope.model.config.removeNaviHideItems == true 
+      ? item.naviHide !== true 
+      : true;
   };
-
+  
   $scope.$on("formSubmitting", function (ev, args) {
     $scope.model.value = $scope.items;
   });
-
+  
   // Find entities
   function getItemEntities(items) {
     _.each(items, function (item) {
@@ -69,27 +71,29 @@
         meganavResource.getById(item.id).then(function (response) {
           angular.extend(item, response.data);
         });
-
+        
         if (item.children.length > 0) {
           getItemEntities(item.children);
         }
       }
     });
   }
-
+  
   // Open settings overlay
   function openSettings(item, callback) {
     // Update Properties appropriately
     if (!angular.isObject(item.properties)) {
       item.properties = {};
     }
-
+    
     // Get available property fields
-    var props = angular.copy(propertyFields).filter(isPropertyFieldEnabled, item).map(setPropertyFieldValue, item);
-
+    var props = angular.copy(propertyFields)
+      .filter(isPropertyFieldEnabled, item)
+      .map(setPropertyFieldValue, item);
+    
     // Clone original item so our changes aren't live
     clonedItem = angular.copy(item);
-
+    
     // Assign value to new empty object to break refs
     // Prevent accidentally auto changing old values
     $scope.settingsOverlay = {
@@ -102,19 +106,19 @@
         // Update Properties
         model.value.properties = getItemProperties(model.value, model.properties);
         !callback || callback(model);
-
+        
         // close settings
         closeSettings();
       }
     }
   }
-
+  
   // Close settings overlay
   function closeSettings() {
     $scope.settingsOverlay.show = false;
     $scope.settingsOverlay = null;
   }
-
+  
   // Create/Update Nav Item
   function buildNavItem(data, old) {
     var defaults = {
@@ -131,7 +135,7 @@
     };
     return angular.extend(old || defaults, data);
   }
-
+  
   // Check if property field should be enabled for current item
   function isPropertyFieldEnabled(prop) {
     return (
@@ -141,22 +145,20 @@
       prop.applyTo.indexOf(this.level || 0) >= 0
     );
   }
-
+  
   // Set the items values on the property field
   function setPropertyFieldValue(prop) {
     var val = this.properties[prop.key];
     prop.value = val;
     return prop;
   }
-
+  
   // Get the properties for an item from an array of property fields
   function getItemProperties(item, props) {
     props = _.isEmpty(props) ? propertyFields : props;
-    return angular.extend({},
-      ...props
-        .filter(isPropertyFieldEnabled, item)
-        .map(x => ({[x.key]: x.value }))
-    );
+    return props
+      .filter(isPropertyFieldEnabled, item)
+      .reduce(function(props, p) { return (props[p.key] = p.value, props) }, {})
   }
 }
 
