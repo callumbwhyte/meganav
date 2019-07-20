c6955f4b197c2bea95733caa895891947e45dec8
Callum Whyte <hey@callumwhyte.com>
2019-07-20 15:21:07 +0100

    Value Editor converts model for backoffice and database

--- src/Our.Umbraco.Meganav/PropertyEditors/MeganavPropertyEditor.cs
+++ src/Our.Umbraco.Meganav/PropertyEditors/MeganavPropertyEditor.cs
@@ -1,17 +1,22 @@
 ﻿using Umbraco.Core.Logging;
 using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
 
 namespace Our.Umbraco.Meganav.PropertyEditors
 {
     [DataEditor(Constants.PropertyEditorAlias, Constants.PropertyEditorName, Group = "pickers")]
     internal class MeganavPropertyEditor : DataEditor
     {
-        public MeganavPropertyEditor(ILogger logger)
+        private readonly IContentService _contentService;
+
+        public MeganavPropertyEditor(IContentService contentService, ILogger logger)
             : base(logger, EditorType.PropertyValue)
         {
-
+            _contentService = contentService;
         }
 
         protected override IConfigurationEditor CreateConfigurationEditor() => new MeganavConfigurationEditor();
+
+        protected override IDataValueEditor CreateValueEditor() => new MeganavValueEditor(_contentService);
     }
 }
---
+++ src/Our.Umbraco.Meganav/PropertyEditors/MeganavValueEditor.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Our.Umbraco.Meganav.Models;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.Editors;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+using Umbraco.Web.Composing;
+
+namespace Our.Umbraco.Meganav.PropertyEditors
+{
+    internal class MeganavValueEditor : DataValueEditor
+    {
+        private readonly IContentService _contentService;
+
+        public MeganavValueEditor(IContentService contentService)
+        {
+            ValueType = "JSON";
+
+            _contentService = contentService;
+        }
+
+        public override object ToEditor(Property property, IDataTypeService dataTypeService, string culture = null, string segment = null)
+        {
+            var data = property.GetValue(culture, segment);
+
+            if (!(data is string value))
+            {
+                return null;
+            }
+
+            var entities = JsonConvert.DeserializeObject<IEnumerable<MeganavEditorEntity>>(value);
+
+            EnrichEntities(Current.UmbracoContext, entities, culture);
+
+            return entities;
+        }
+
+        public override object FromEditor(ContentPropertyData editorValue, object currentValue)
+        {
+            if (!(editorValue.Value is JToken value))
+            {
+                return null;
+            }
+
+            var entities = value.ToObject<IEnumerable<MeganavEntity>>();
+
+            SanitizeEntities(entities);
+
+            return JsonConvert.SerializeObject(entities);
+        }
+
+        private void EnrichEntities(UmbracoContext umbracoContext, IEnumerable<MeganavEditorEntity> entities, string culture)
+        {
+            foreach (var entity in entities)
+            {
+                if (entity.Udi != null)
+                {
+                    var content = _contentService.GetById(entity.Udi.Guid);
+
+                    if (content == null)
+                    {
+                        continue;
+                    }
+
+                    entity.Name = content.GetCultureName(culture) ?? content.Name;
+
+                    entity.Icon = content.ContentType.Icon;
+
+                    entity.Published = content.IsCulturePublished(culture) || content.Published;
+
+                    entity.Url = umbracoContext?.Content.GetById(content.Id)?.Url(culture);
+                }
+
+                if (entity.Children != null)
+                {
+                    EnrichEntities(umbracoContext, entity.Children, culture);
+                }
+            }
+        }
+
+        private void SanitizeEntities(IEnumerable<MeganavEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                if (entity.Udi != null)
+                {
+                    entity.Url = null;
+                }
+
+                if (entity.Children != null)
+                {
+                    SanitizeEntities(entity.Children);
+                }
+            }
+        }
+    }
+}
