cfa62dc4cbb1b6e66cd99b95109bf51af96d630b
Callum Whyte <hey@callumwhyte.com>
2019-08-01 19:16:24 +0100

    Toggle visibility in nav item actions

--- src/Our.Umbraco.Meganav/Web/UI/App_Plugins/Meganav/backoffice/components/meganavActions.component.js
+++ src/Our.Umbraco.Meganav/Web/UI/App_Plugins/Meganav/backoffice/components/meganavActions.component.js
@@ -10,7 +10,23 @@
                 onRemove: "&?"
             },
             templateUrl: "/App_Plugins/Meganav/backoffice/components/meganavActions.html",
-            controllerAs: "vm"
+            controllerAs: "vm",
+            controller: function () {
+                var vm = this;
+
+                vm.toggleVisibility = function () {
+                    var toggleChildren = function (item) {
+                        item.children.forEach(child => {
+                            child.visible = item.visible;
+                            toggleChildren(child);
+                        });
+                    };
+                    // set visibility
+                    vm.item.visible = !vm.item.visible;
+                    // apply to children
+                    toggleChildren(vm.item);
+                };
+            }
         });
 
 })();
