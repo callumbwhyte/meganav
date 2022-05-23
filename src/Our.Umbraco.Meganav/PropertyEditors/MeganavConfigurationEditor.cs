27c2de2900922aa4fcd644fcc4faed9f04e8a3f4
Callum Whyte <hey@callumwhyte.com>
2022-05-23 20:23:25 +0100

    Setting default item type icon in configuration editor

--- src/Our.Umbraco.Meganav/PropertyEditors/MeganavConfigurationEditor.cs
+++ src/Our.Umbraco.Meganav/PropertyEditors/MeganavConfigurationEditor.cs
@@ -1,9 +1,28 @@
-﻿using Umbraco.Core.PropertyEditors;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Our.Umbraco.Meganav.Models;
+using Umbraco.Core.PropertyEditors;
 
 namespace Our.Umbraco.Meganav.PropertyEditors
 {
     internal class MeganavConfigurationEditor : ConfigurationEditor<MeganavConfiguration>
     {
+        public override IDictionary<string, object> ToValueEditor(object configuration)
+        {
+            var value = base.ToValueEditor(configuration);
 
+            if (value.TryGetValue("itemTypes", out var data) == true)
+            {
+                if (data is IEnumerable<IMeganavItemType> itemTypes)
+                {
+                    foreach (var itemType in itemTypes.OfType<MeganavItemType>())
+                    {
+                        itemType.Icon = itemType.Icon ?? "icon-link";
+                    }
+                }
+            }
+
+            return value;
+        }
     }
 }
